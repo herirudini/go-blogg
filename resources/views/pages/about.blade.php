6e2d39d8aeb967269f97c3c2de66739372260068
herirudini <herirudini@gmail.com>
2025-08-06 23:19:53 +0700

    update(about, contact, article): update UI

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.main')
- 
+
 @section('content')
-    <h2 class="text-3xl font-bold">About Us</h2>
-    <p class="mt-4 text-lg text-gray-600">We're the best company in the world!</p>
+    <x-card>
+        <h2 class="text-3xl font-bold">About Us</h2>
+        <p class="mt-4 text-lg text-gray-600">We're the best company in the world!</p>
+    </x-card>
 @endsection
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.main')
  
 @section('content')
-    <h2 class="text-3xl font-bold">Contact Us</h2>
-    <p class="mt-4 text-lg text-gray-600">Email: support@website.com</p>
+    <x-card>
+        <h2 class="text-3xl font-bold">Contact Us</h2>
+        <p class="mt-4 text-lg text-gray-600">Email: support@website.com</p>
+    </x-card>
 @endsection
