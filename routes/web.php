bb9f386f2f745defb88e2b125471768770242ede
herirudini <herirudini@gmail.com>
2025-08-08 21:56:06 +0700

    update(web): change routing method with ::get to do more task which is query GET data from database

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -9,7 +10,12 @@ use Illuminate\Support\Facades\Route;
 // // More simple syntax: ideal for static pages
 // Route::view('/second', 'second'); 
 
-Route::view('/', 'pages.home');
+// Route::view('/', 'pages.home');
 Route::view('/contact-us', 'pages.contact')->name('contact');
 Route::view('/about-us', 'pages.about')->name('about');
 Route::view('/article/{id}', 'pages.article')->name('article');
+
+Route::get('/', function () {
+    $categories = DB::table('categories')->get();
+    return view('pages.home', ['categories' => $categories]); //we can use the categories in the component as data source
+});
