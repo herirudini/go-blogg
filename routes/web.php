580e085136a1a29fd9589a091c5cfe9cb38bc3bf
herirudini <herirudini@gmail.com>
2025-08-08 22:06:34 +0700

    update(home, web): implement query controller and consume the data

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\DB;
 // // More simple syntax: ideal for static pages
 // Route::view('/second', 'second'); 
 
-Route::view('/', 'pages.home');
+// Route::view('/', 'pages.home');
 Route::view('/contact-us', 'pages.contact')->name('contact');
 Route::view('/about-us', 'pages.about')->name('about');
 Route::view('/article/{id}', 'pages.article')->name('article');
@@ -19,3 +19,4 @@ Route::view('/article/{id}', 'pages.article')->name('article');
 //     $categories = DB::table('categories')->get();
 //     return view('pages.home', ['categories' => $categories]); //we can use the categories in the component as data source
 // });
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home'); 
